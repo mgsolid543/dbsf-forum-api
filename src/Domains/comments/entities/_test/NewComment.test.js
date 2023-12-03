c9b1ef2af4524f49906759e9d0abbea6a0f34827
Irpan Budiana <irpan.budiana@gmail.com>
2023-12-03 23:03:32 +0700

    new comment test

---
+++ src/Domains/comments/entities/_test/NewComment.test.js
@@ -0,0 +1,44 @@
+const NewComment = require("../NewComment");
+
+describe("a NewComment entities", () => {
+    it("should throw error when payload did not contain needed property", () => {
+        // Arrange
+        const payload = {
+            contents: "Example Content",
+        };
+
+        // Action and Assert
+        expect(() => new NewComment(payload)).toThrowError(
+            "NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY"
+        );
+    });
+
+    it("should throw error when payload did not meet data type specification", () => {
+        // Arrange
+        const payload = {
+            threadId: 'thread-123',
+            content: true,
+            owner: 'user-123',
+        };
+
+        // Action and Assert
+        expect(() => new NewComment(payload)).toThrowError(
+            "NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION"
+        );
+    });
+
+    it("should create newComment object correctly", () => {
+        // Arrange
+        const payload = {
+            threadId: 'thread-123',
+            content: 'Example content',
+            owner: 'user-123',
+        };
+
+        // Action
+        const { content } = new NewComment(payload);
+
+        // Assert
+        expect(content).toEqual(payload.content);
+    });
+});
